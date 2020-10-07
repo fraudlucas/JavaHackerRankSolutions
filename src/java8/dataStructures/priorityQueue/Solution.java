4e85ad81be1d5cd4331930d3a15d3232503cdfbd
Marcus Lucas <marcus_lucas12@outlook.com>
2020-10-07 11:23:42 -0300

    Java Priority Queue problem solution

---
+++ src/java8/dataStructures/priorityQueue/Solution.java
@@ -0,0 +1,106 @@
+package java8.dataStructures.priorityQueue;
+
+
+import java.util.*;
+
+/*
+ * Create the Student and Priorities classes here.
+ */
+class Priorities {
+
+    //    PriorityQueue<Student> students = new PriorityQueue<>();
+    PriorityQueue<Student> students = new PriorityQueue<>(
+            Comparator.comparing(Student::getCgpa).reversed()
+                    .thenComparing(Student::getName)
+                    .thenComparing(Student::getId)
+    );
+
+    static final int EVENT = 0;
+    static final int NAME = 1;
+    static final int CGPA = 2;
+    static final int ID = 3;
+
+
+    public List<Student> getStudents(List<String> events) {
+
+        for (String event : events) {
+            String[] e = event.split(" ");
+
+            switch (e[EVENT]) {
+                case "ENTER":
+                    Student student = new Student(Integer.parseInt(e[ID]), e[NAME],
+                            Double.parseDouble(e[CGPA]));
+                    this.students.add(student);
+                    break;
+                case "SERVED":
+                    this.students.poll();
+                    break;
+            }
+        }
+
+        ArrayList<Student> students = new ArrayList<>(this.students);
+        Collections.sort(students);
+        return students;
+    }
+}
+
+class Student implements Comparable<Student> {
+
+    private final int id;
+    private final String name;
+    private final double cgpa;
+
+    public Student(int id, String name, double cgpa) {
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getCgpa() {
+        return this.cgpa;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        if (this.cgpa > other.getCgpa()) return -1;
+        if (this.cgpa < other.getCgpa()) return 1;
+
+        int compare = this.name.compareTo(other.getName());
+        if (compare != 0) return compare;
+
+        return other.getId() - this.id;
+    }
+}
+
+public class Solution {
+    private final static Scanner scan = new Scanner(System.in);
+    private final static Priorities priorities = new Priorities();
+
+    public static void main(String[] args) {
+        int totalEvents = Integer.parseInt(scan.nextLine());
+        List<String> events = new ArrayList<>();
+
+        while (totalEvents-- != 0) {
+            String event = scan.nextLine();
+            events.add(event);
+        }
+
+        List<Student> students = priorities.getStudents(events);
+
+        if (students.isEmpty()) {
+            System.out.println("EMPTY");
+        } else {
+            for (Student st : students) {
+                System.out.println(st.getId() + " " + st.getName() + " " + st.getId());
+            }
+        }
+    }
+}
